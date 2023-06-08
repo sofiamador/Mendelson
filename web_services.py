2e728bc7c77bee5bd5438aa004318a7a99ea3e2e
sofiamador <sofiamador@gmail.com>
2023-06-08 15:26:02 +0300

    api main

--- web_services.py
+++ web_services.py
@@ -2,13 +2,14 @@ import requests
 import json
 
 from Enteties import GroupOfOrders
-
+host = "https://menprime.mendelson.co.il/odata/Priority/tabula.ini/a121204/"
+#host = https://priweb.mendelson.co.il/odata/Priority/tabula.ini/a121204/
 with open('auth.txt', 'rb') as f:
     auth = f.read()
 
 
 def get_stock():
-    url = "https://priweb.mendelson.co.il/odata/Priority/tabula.ini/a121204/PARTBAL?$select=PARTNAME, LOCNAME, STZONECODE,BALANCE,ACTNAME&$filter=WARHSNAME eq '500' and (STZONECODE eq 'C1' or STZONECODE eq 'W1' or STZONECODE eq 'W2') and BALANCE ne 0 AND CUSTNAME eq 'Goods'"
+    url = host+"PARTBAL?$select=PARTNAME, LOCNAME, STZONECODE,BALANCE,ACTNAME&$filter=WARHSNAME eq '500' and (STZONECODE eq 'C1' or STZONECODE eq 'W1' or STZONECODE eq 'W2') and BALANCE ne 0 AND CUSTNAME eq 'Goods'"
 
     payload = {}
     headers = {
@@ -30,7 +31,7 @@ def get_stock2():
 
 
 def get_wtasks():
-    url = "https://priweb.mendelson.co.il/odata/Priority/tabula.ini/a121204/WTASKS?$select=STZONECODE,WTASKNUM,PRIO,DOERLOGIN,STATDES,ADCSTARTED,WTASKTYPECODE&$filter=WARHSNAME eq '500' and CDES ne 'סניף*' and(STZONECODE eq 'C1' or STZONECODE eq 'W1' or STZONECODE eq 'W2') and DOERLOGIN ne 'dimitri' and (STATDES eq 'לביצוע' or STATDES eq 'מושהה') and (WTASKTYPECODE eq 'PIK' or WTASKTYPECODE eq 'RPI' or WTASKTYPECODE eq 'RPL' or WTASKTYPECODE eq 'MOV' or WTASKTYPECODE eq 'PUT') and ADCSTARTED ne 'Y'&$expand=WTASKITEMS_SUBFORM($select=PARTNAME, LOCNAME, PTQUANT,KLINE)"
+    url = host+"WTASKS?$select=STZONECODE,WTASKNUM,PRIO,DOERLOGIN,STATDES,ADCSTARTED,WTASKTYPECODE&$filter=WARHSNAME eq '500' and CDES ne 'סניף*' and(STZONECODE eq 'C1' or STZONECODE eq 'W1' or STZONECODE eq 'W2') and DOERLOGIN ne 'dimitri' and (STATDES eq 'לביצוע' or STATDES eq 'מושהה') and (WTASKTYPECODE eq 'PIK' or WTASKTYPECODE eq 'RPI' or WTASKTYPECODE eq 'RPL' or WTASKTYPECODE eq 'MOV' or WTASKTYPECODE eq 'PUT') and ADCSTARTED ne 'Y'&$expand=WTASKITEMS_SUBFORM($select=PARTNAME, LOCNAME, PTQUANT,KLINE)"
     payload = {}
     headers = {
         'X-App-Id': 'APPSS04',
@@ -53,7 +54,7 @@ def post_transfer_tasks(schedule):
     for k, tasks in schedule.items():
         if len(tasks) == 0:
             continue
-        url = "https://priweb.mendelson.co.il/odata/Priority/tabula.ini/a121204/WTASKS"
+        url = host+"WTASKS"
         payload = {
             "WTASKTYPECODE": "MOV",
             "WARHSNAME": "500",
@@ -96,7 +97,7 @@ def patch_update_allocation(schedule):
         for t in tasks:
             if isinstance(t,GroupOfOrders):
                 for order in t.orders:
-                    url = "https://priweb.mendelson.co.il/odata/Priority/tabula.ini/a121204/WTASKS('" + order.order_id + "')"
+                    url = host+"WTASKS('" + order.order_id + "')"
                     payload = json.dumps({
                         "DOERLOGIN": k
                     })
@@ -110,7 +111,7 @@ def patch_update_allocation(schedule):
                     f.write(str(payload) + "\n")
                     # response = requests.request("PATCH", url, headers=headers, data=payload)
             else:
-                url = "https://priweb.mendelson.co.il/odata/Priority/tabula.ini/a121204/WTASKS('" + t.order_id + "')"
+                url = host+"WTASKS('" + t.order_id + "')"
                 payload = json.dumps({
                     "DOERLOGIN": k
                 })
@@ -131,13 +132,12 @@ def patch_update_allocation(schedule):
 
 def patch_upadate_location_for_items(schedule):
     f = open("update_allocation.txt", "w")
-    url = "https://priweb.mendelson.co.il/odata/Priority/tabula.ini/a121204/WTASKS('')"
     for k, tasks in schedule.items():
         if len(tasks) == 0:
             continue
         for task in tasks:
             for item in task.lines:
-                url = "https://priweb.mendelson.co.il/odata/Priority/tabula.ini/a121204/WTASKS('" + 11111 + "')"  # TODO BEN
+                url = host+"WTASKS('" + 11111 + "')"  # TODO BEN
 
                 payload = json.dumps({
                     "WTASKITEMS_SUBFORM": [
