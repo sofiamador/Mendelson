7321bffc424bb4c0591a0d27229107d72fc43d33
sofiamador <sofiamador@gmail.com>
2023-09-03 11:11:32 +0300

    employees update

--- functions.py
+++ functions.py
@@ -201,7 +201,7 @@ def create_lines_from_json_after_gal(lines_input_):
                 location_string = item["LOCNAME"]
                 line_number = item["KLINE"]
 
-                line = Line(item_id, order_id, quantity, warehouse_id, location_string, line_number, priority)
+                line = Line(item_id, order_id, quantity, warehouse_id, location_string, line_number, priority,is_store=is_store)
                 if line.location.warehouse_id== "C1" and line.location.street.isdigit() and line.location.row.isdigit():
                     street_number = int(line.location.street)
                     row_number = int(line.location.row)
