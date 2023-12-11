aa1b74c90dd200a55743ce4f7e66cea2aa248e31
sofiamador <sofiamador@gmail.com>
2023-12-11 09:20:52 +0200

    employees update

--- Globals.py
+++ Globals.py
@@ -8,7 +8,7 @@ is_with_transfer = False
 
 # employees
 time_to_ignore_employee = 1
-employees_to_ignore= ["hasana", "rami","meirg","chaimh"]
+employees_to_ignore= ["hasana", "rami","meirg","chaimh","gavriel"]
 number_of_allocation_per_employee = 5
 #host = "https://menprime.mendelson.co.il/odata/Priority/tabula.ini/a121204/"  #test
 host = "https://priweb.mendelson.co.il/odata/Priority/tabula.ini/a121204/"  #main
